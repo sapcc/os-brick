9393c59094ffb14a244435fc88e2c302cae7506d
Marius Leustean <marius.leus@gmail.com>
2020-01-07 11:57:38 +0200

    [vmware] add support for disconnecting a volume via HttpNfc upload
    os-brick needs to be able to upload a vmdk file via HttpNfc upload
    after obtaining a lease from ImportVApp operation. This will be
    used in a situation like restoring a VVol volume from a backup.
    os-brick will receive information from cinder-volume of how to
    construct the ImportVApp call to VMware API, via import_data.
    This is backward compatible with the initial method of uploading
    the vmdk to VMWare.
    
    (cherry picked from commit 4558baf5439772025d2bb0207ab7c4cca8057685)

--- os_brick/tests/initiator/connectors/test_vmware.py
+++ os_brick/tests/initiator/connectors/test_vmware.py
@@ -38,6 +38,7 @@ class VmdkConnectorTestCase(test_connector.ConnectorTestCase):
     IMG_TX_TIMEOUT = 10
 
     VMDK_CONNECTOR = vmware.VmdkConnector
+    VMDK_VOLUMEOPS = vmware.VolumeOps
 
     def setUp(self):
         super(VmdkConnectorTestCase, self).setUp()
@@ -149,6 +150,8 @@ class VmdkConnectorTestCase(test_connector.ConnectorTestCase):
         return {'volume_id': 'ed083474-d325-4a99-b301-269111654f0d',
                 'volume': 'ref-1',
                 'vmdk_path': '[ds] foo/bar.vmdk',
+                'profile_id': 'profile-1',
+                'name': 'volume-name-001',
                 'vmdk_size': units.Gi,
                 'datastore': 'ds-1',
                 'datacenter': 'dc-1',
@@ -218,26 +221,20 @@ class VmdkConnectorTestCase(test_connector.ConnectorTestCase):
 
     @mock.patch('oslo_vmware.objects.datastore.get_datastore_by_ref')
     @mock.patch.object(VMDK_CONNECTOR, '_create_temp_ds_folder')
-    @mock.patch('os_brick.initiator.connectors.vmware.open', create=True)
     @mock.patch.object(VMDK_CONNECTOR, '_upload_vmdk')
-    @mock.patch('os.path.getsize')
     @mock.patch.object(VMDK_CONNECTOR, '_get_disk_device')
     @mock.patch.object(VMDK_CONNECTOR, '_detach_disk_from_backing')
     @mock.patch.object(VMDK_CONNECTOR, '_attach_disk_to_backing')
     def test_disconnect(
             self, attach_disk_to_backing, detach_disk_from_backing,
-            get_disk_device, getsize, upload_vmdk, file_open,
-            create_temp_ds_folder, get_ds_by_ref):
+            get_disk_device, upload_vmdk, create_temp_ds_folder,
+            get_ds_by_ref):
         ds_ref = mock.sentinel.ds_ref
         ds_name = 'datastore-1'
         dstore = datastore.Datastore(ds_ref, ds_name)
         get_ds_by_ref.return_value = dstore
 
-        file_open_ret = mock.Mock()
         tmp_file = mock.sentinel.tmp_file
-        file_open_ret.__enter__ = mock.Mock(return_value=tmp_file)
-        file_open_ret.__exit__ = mock.Mock(return_value=None)
-        file_open.return_value = file_open_ret
 
         dc_name = mock.sentinel.dc_name
         copy_task = mock.sentinel.copy_vdisk_task
@@ -245,7 +242,6 @@ class VmdkConnectorTestCase(test_connector.ConnectorTestCase):
         session = mock.Mock()
         session.invoke_api.side_effect = [dc_name, copy_task, delete_file_task]
 
-        getsize.return_value = units.Gi
         disk_device = mock.sentinel.disk_device
         get_disk_device.return_value = disk_device
 
@@ -253,14 +249,15 @@ class VmdkConnectorTestCase(test_connector.ConnectorTestCase):
         tmp_file_path = '/tmp/foo.vmdk'
         dc_ref = mock.sentinel.dc_ref
         vmdk_path = mock.sentinel.vmdk_path
+        file_size = 1024
         self._connector._disconnect(
-            backing, tmp_file_path, session, ds_ref, dc_ref, vmdk_path)
+            backing, session, tmp_file_path, tmp_file, file_size, ds_ref,
+            dc_ref, vmdk_path)
 
         tmp_folder_path = self._connector.TMP_IMAGES_DATASTORE_FOLDER_PATH
         ds_folder_path = '[%s] %s' % (ds_name, tmp_folder_path)
         create_temp_ds_folder.assert_called_once_with(
             session, ds_folder_path, dc_ref)
-        file_open.assert_called_once_with(tmp_file_path, "rb")
 
         self.assertEqual(
             mock.call(vim_util, 'get_object_property', session.vim, dc_ref,
@@ -270,7 +267,7 @@ class VmdkConnectorTestCase(test_connector.ConnectorTestCase):
         upload_vmdk.assert_called_once_with(
             tmp_file, self._connector._ip, self._connector._port, dc_name,
             ds_name, session.vim.client.options.transport.cookiejar,
-            exp_rel_path, units.Gi, self._connector._ca_file,
+            exp_rel_path, file_size, self._connector._ca_file,
             self._connector._timeout)
 
         get_disk_device.assert_called_once_with(session, backing)
@@ -297,6 +294,47 @@ class VmdkConnectorTestCase(test_connector.ConnectorTestCase):
             session.invoke_api.call_args_list[2])
         self.assertEqual(mock.call(delete_file_task),
                          session.wait_for_task.call_args_list[1])
+
+    @mock.patch('oslo_vmware.image_transfer._start_transfer')
+    @mock.patch('oslo_vmware.rw_handles.VmdkWriteHandle')
+    @mock.patch('oslo_vmware.vim_util.get_moref')
+    @mock.patch('os_brick.initiator.connectors.vmware.VolumeOps')
+    def test_disconnect_with_import(self, vops, get_moref, vmdk_write_handle,
+                                    start_transfer):
+        import_spec = mock.Mock()
+        session = mock.sentinel.session
+        backing = mock.sentinel.backing
+        tmp_file = mock.sentinel.file
+        file_size = 1024
+        import_data = {
+            'folder': mock.sentinel.folder,
+            'resource_pool': mock.sentinel.resource_pool
+        }
+        vops._session = mock.sentinel._session
+        vops.get_import_spec.return_value = import_spec
+        get_moref.side_effect = [import_data['folder'], import_data[
+            'resource_pool']]
+        vmdk_write_handle_ret = mock.sentinel.vmdk_write_handle
+        vmdk_write_handle.return_value = vmdk_write_handle_ret
+
+        self._connector._disconnect_with_import(import_data, vops,
+                                                backing, tmp_file, file_size)
+
+        vops.delete_backing.assert_called_once_with(backing)
+
+        vmdk_write_handle.assert_called_once_with(vops._session,
+                                    self._connector._ip,
+                                    self._connector._port,
+                                    import_data['resource_pool'],
+                                    import_data['folder'],
+                                    import_spec,
+                                    file_size,
+                                    'POST')
+
+        start_transfer.assert_called_once_with(tmp_file,
+                                               vmdk_write_handle_ret,
+                                               self._connector._timeout)
+
 
     @mock.patch('os.path.exists')
     def test_disconnect_volume_with_missing_temp_file(self, path_exists):
@@ -329,6 +367,9 @@ class VmdkConnectorTestCase(test_connector.ConnectorTestCase):
         disconnect.assert_not_called()
         remove.assert_called_once_with(path)
 
+    @mock.patch('os_brick.initiator.connectors.vmware.VolumeOps')
+    @mock.patch('os_brick.initiator.connectors.vmware.open', create=True)
+    @mock.patch('os.path.getsize')
     @mock.patch('os.path.exists')
     @mock.patch('os.path.getmtime')
     @mock.patch.object(VMDK_CONNECTOR, '_load_config')
@@ -337,11 +378,15 @@ class VmdkConnectorTestCase(test_connector.ConnectorTestCase):
     @mock.patch.object(VMDK_CONNECTOR, '_snapshot_exists')
     @mock.patch.object(VMDK_CONNECTOR, '_disconnect')
     @mock.patch('os.remove')
+    @mock.patch.object(VMDK_CONNECTOR, '_disconnect_with_import')
     def test_disconnect_volume(
-            self, remove, disconnect, snapshot_exists, get_moref,
-            create_session, load_config, getmtime, path_exists):
-        path_exists.return_value = True
+            self, disconnect_with_import, remove, disconnect, snapshot_exists,
+            get_moref, create_session, load_config, getmtime, path_exists,
+            path_getsize, open_file, vops, import_data=False):
 
+        path_exists.return_value = True
+        file_size = 1024
+        path_getsize.return_value = file_size
         mtime = 1467802060
         getmtime.return_value = mtime
 
@@ -356,19 +401,184 @@ class VmdkConnectorTestCase(test_connector.ConnectorTestCase):
         get_moref.side_effect = [backing, ds_ref, dc_ref]
 
         props = self._create_connection_properties()
+        if import_data:
+            props['import_data'] = {'vm': {}}
+
         path = mock.sentinel.path
+        tmp_file = mock.sentinel.tmp_file
+        file_open_ret = mock.Mock()
+        file_open_ret.__enter__ = mock.Mock(return_value=tmp_file)
+        file_open_ret.__exit__ = mock.Mock(return_value=None)
+        open_file.return_value = file_open_ret
+        vops_ret = mock.Mock(_session=session)
+        vops.return_value = vops_ret
+
         self._connector.disconnect_volume(props, {'path': path,
                                                   'last_modified': mtime - 1})
-
         path_exists.assert_called_once_with(path)
         getmtime.assert_called_once_with(path)
+        open_file.assert_called_once_with(path, "rb")
         load_config.assert_called_once_with(props)
         create_session.assert_called_once_with()
         snapshot_exists.assert_called_once_with(session, backing)
-        disconnect.assert_called_once_with(
-            backing, path, session, ds_ref, dc_ref, props['vmdk_path'])
+        path_getsize.assert_called_once_with(path)
+        if import_data:
+            disconnect.assert_not_called()
+            disconnect_with_import.assert_called_once_with(
+                props['import_data'], vops_ret, backing, tmp_file, file_size)
+        else:
+            disconnect.assert_called_once_with(
+            backing, session, path, tmp_file, file_size, ds_ref, dc_ref,
+                props['vmdk_path'])
+            disconnect_with_import.assert_not_called()
         remove.assert_called_once_with(path)
         session.logout.assert_called_once_with()
+
+    def test_disconnect_volume_with_import(self):
+        self.test_disconnect_volume(import_data=True)
+
+    @staticmethod
+    def _create_import_data(folder=None, rp=None):
+        return {
+            'profile_id': 'profile-1',
+            'folder': folder.value if folder else mock.Mock(),
+            'resource_pool': rp.value if rp else mock.Mock(),
+            'vm': {
+                'name': 'vm-1',
+                'uuid': '0-1-2-3',
+                'path_name': '[ds-1]',
+                'guest_id': 'guest-id',
+                'num_cpus': 1,
+                'memory_mb': 128,
+                'vmx_version': 'vmx-8',
+                'extension_key': 'foo-extension-key',
+                'extension_type': 'foo-extension-type',
+                'extra_config': {'foo': 'bar'}
+            },
+            'adapter_type': mock.Mock(),
+            'controller': {
+                'type': 'controllerTypeOne',
+                'key': 1,
+                'create': True,
+                'shared_bus': 'shared',
+                'bus_number': 1
+            },
+            'disk': {
+                'type': 'diskTypeOne',
+                'key': -101,
+                'capacity_in_kb': 1024 * 1024,
+                'eagerly_scrub': None,
+                'thin_provisioned': True
+            }
+        }
+
+    def test_volumeops_get_vm_config_spec(self):
+        session = mock.Mock()
+        cf = mock.Mock()
+        session.vim.client.factory = cf
+
+        vm_file_info = mock.Mock()
+        config_spec = mock.Mock()
+        extra_config = mock.Mock()
+        vm_profile = mock.Mock()
+        managed_by = mock.Mock()
+        controller_device = mock.Mock()
+        controller_spec = mock.Mock()
+        disk_device = mock.Mock()
+        disk_backing = mock.Mock()
+        disk_spec = mock.Mock()
+        disk_profile = mock.Mock()
+
+        cf.create.side_effect = [vm_file_info, config_spec, extra_config,
+                                 vm_profile, managed_by, controller_device,
+                                 controller_spec, disk_device, disk_backing,
+                                 disk_spec, disk_profile]
+
+        data = self._create_import_data()
+
+        vops = vmware.VolumeOps(session)
+        spec = vops.get_vm_config_spec(data)
+
+        cf.create.assert_has_calls([
+            mock.call('ns0:VirtualMachineFileInfo'),
+            mock.call('ns0:VirtualMachineConfigSpec'),
+            mock.call('ns0:OptionValue'),
+            mock.call('ns0:VirtualMachineDefinedProfileSpec'),
+            mock.call('ns0:ManagedByInfo'),
+            mock.call('ns0:%s' % data['controller']['type']),
+            mock.call('ns0:VirtualDeviceConfigSpec'),
+            mock.call('ns0:VirtualDisk'),
+            mock.call('ns0:VirtualDiskFlatVer2BackingInfo'),
+            mock.call('ns0:VirtualDeviceConfigSpec'),
+            mock.call('ns0:VirtualMachineDefinedProfileSpec'),
+        ])
+
+        vm = data['vm']
+        self.assertEqual(spec.files.vmPathName, vm['path_name'])
+        self.assertEqual(spec.guestId, vm['guest_id'])
+        self.assertEqual(spec.numCPUs, vm['num_cpus'])
+        self.assertEqual(spec.memoryMB, vm['memory_mb'])
+        self.assertEqual(spec.files, vm_file_info)
+        self.assertEqual(spec.version, vm['vmx_version'])
+        self.assertEqual(spec.instanceUuid, vm['uuid'])
+        self.assertEqual(spec.extraConfig, [extra_config])
+        self.assertEqual(spec.vmProfile, vm_profile)
+        self.assertEqual(spec.vmProfile.profileId, data['profile_id'])
+        self.assertEqual(spec.managedBy, managed_by)
+        self.assertEqual(extra_config.key, 'foo')
+        self.assertEqual(extra_config.value, 'bar')
+        self.assertEqual(spec.deviceChange, [disk_spec, controller_spec])
+
+        disk = data['disk']
+        ctrl = data['controller']
+
+        self.assertEqual(controller_spec.device, controller_device)
+        self.assertEqual(controller_device.key, ctrl['key'])
+        self.assertEqual(controller_device.busNumber, ctrl['bus_number'])
+        self.assertEqual(controller_device.sharedBus, ctrl['shared_bus'])
+
+        self.assertEqual(disk_device.capacityInKB, disk['capacity_in_kb'])
+        self.assertEqual(disk_device.key, disk['key'])
+        self.assertEqual(disk_device.unitNumber, 0)
+        self.assertEqual(disk_device.controllerKey, ctrl['key'])
+
+        self.assertEqual(disk_device.backing, disk_backing)
+        self.assertEqual(disk_backing.thinProvisioned, True)
+        self.assertEqual(disk_backing.fileName, '')
+        self.assertEqual(disk_backing.diskMode, 'persistent')
+
+        self.assertEqual(disk_spec.operation, 'add')
+        self.assertEqual(disk_spec.fileOperation, 'create')
+        self.assertEqual(disk_spec.device, disk_device)
+        self.assertEqual(disk_spec.profile, [disk_profile])
+        self.assertEqual(disk_profile.profileId, data['profile_id'])
+
+    @mock.patch.object(VMDK_VOLUMEOPS, 'get_vm_config_spec')
+    def test_volumeops_get_import_spec(self, get_vm_config_spec):
+        session = mock.Mock()
+        cf = mock.Mock()
+        session.vim.client.factory = cf
+        config_spec = mock.Mock()
+        get_vm_config_spec.return_value = config_spec
+
+        vops = vmware.VolumeOps(session)
+        spec = vops.get_import_spec(self._create_import_data())
+
+        cf.create.assert_called_once_with('ns0:VirtualMachineImportSpec')
+        self.assertEquals(spec.configSpec, config_spec)
+
+    def test_volumeops_delete_backing(self):
+        session = mock.Mock(vim=mock.Mock())
+        backing = mock.Mock()
+        task = mock.Mock()
+        session.invoke_api.return_value = task
+
+        vops = vmware.VolumeOps(session)
+        vops.delete_backing(backing)
+
+        session.invoke_api.assert_called_once_with(session.vim,
+                                                   'Destroy_Task', backing)
+        session.wait_for_task.assert_called_once_with(task)
 
     def test_get_disk_device(self):
         disk_device = mock.Mock()
