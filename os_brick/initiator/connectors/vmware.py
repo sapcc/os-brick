4f1137c83ff091a7b43522cf5acbfcc5dc5e36ed
Marius Leustean <marius.leustean@sap.com>
2020-01-13 18:47:56 +0200

    use vmdk handles for read/write

--- os_brick/initiator/connectors/vmware.py
+++ os_brick/initiator/connectors/vmware.py
@@ -13,11 +13,7 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
-import os
-import tempfile
-
 from oslo_log import log as logging
-from oslo_utils import fileutils
 try:
     from oslo_vmware import api
     from oslo_vmware import exceptions as oslo_vmw_exceptions
@@ -27,7 +23,6 @@ try:
     from oslo_vmware import vim_util
 except ImportError:
     vim_util = None
-import six
 
 from os_brick import exception
 from os_brick.i18n import _
@@ -111,49 +106,41 @@ class VmdkConnector(initiator_connector.InitiatorConnector):
                                     cacert=self._ca_file,
                                     insecure=self._insecure)
 
-    def _create_temp_file(self, *args, **kwargs):
-        fileutils.ensure_tree(self._tmp_dir)
-        fd, tmp = tempfile.mkstemp(dir=self._tmp_dir, *args, **kwargs)
-        os.close(fd)
-        return tmp
-
-    def _download_vmdk(
-            self, tmp_file_path, session, backing, vmdk_path, vmdk_size):
-        with open(tmp_file_path, "wb") as tmp_file:
-            image_transfer.copy_stream_optimized_disk(
-                None,
-                self._timeout,
-                tmp_file,
-                session=session,
-                host=self._ip,
-                port=self._port,
-                vm=backing,
-                vmdk_file_path=vmdk_path,
-                vmdk_size=vmdk_size)
-
     def connect_volume(self, connection_properties):
-        # Download the volume vmdk from vCenter server to a temporary file
-        # and return its path.
         self._load_config(connection_properties)
         session = self._create_session()
 
-        tmp_file_path = self._create_temp_file(
-            suffix=".vmdk", prefix=connection_properties['volume_id'])
-        backing = vim_util.get_moref(connection_properties['volume'],
-                                     "VirtualMachine")
-        vmdk_path = connection_properties['vmdk_path']
+        if connection_properties['import_data']:
+            return self.connect_volume_write_handle(session,
+                                                    connection_properties)
+        else:
+            return self.connect_volume_read_handle(session,
+                                                   connection_properties)
+
+    def connect_volume_read_handle(self, session, connection_properties):
+        vm_ref = vim_util.get_moref(connection_properties['volume'],
+                                    'VirtualMachine')
+
+        handle = rw_handles.VmdkReadHandle(session,
+                                           self._ip,
+                                           self._port,
+                                           vm_ref,
+                                           None,
+                                           connection_properties['vmdk_size'])
+
+        self._read_handles[vm_ref.value] = handle
+
+    def connect_volume_write_handle(self, session, connection_properties):
+        volume_ops = VolumeOps(session)
         vmdk_size = connection_properties['vmdk_size']
-        try:
-            self._download_vmdk(
-                tmp_file_path, session, backing, vmdk_path, vmdk_size)
-        finally:
-            session.logout()
+        import_data = connection_properties['import_data']
+        import_data['profile_id'] = connection_properties['profile_id']
+        import_data['vm']['name'] = "%s_brick" % connection_properties['name']
+        write_handle = self._get_write_handle(import_data, volume_ops,
+                                              vmdk_size)
+        return write_handle
 
-        # Save the last modified time of the temporary so that we can decide
-        # whether to upload the file back to vCenter server during disconnect.
-        last_modified = os.path.getmtime(tmp_file_path)
-        return {'path': tmp_file_path, 'last_modified': last_modified}
-
+    @staticmethod
     def _snapshot_exists(self, session, backing):
         snapshot = session.invoke_api(vim_util,
                                       'get_object_property',
@@ -164,97 +151,14 @@ class VmdkConnector(initiator_connector.InitiatorConnector):
             return False
         return len(snapshot.rootSnapshotList) != 0
 
-    def _create_temp_ds_folder(self, session, ds_folder_path, dc_ref):
-        fileManager = session.vim.service_content.fileManager
-        try:
-            session.invoke_api(session.vim,
-                               'MakeDirectory',
-                               fileManager,
-                               name=ds_folder_path,
-                               datacenter=dc_ref)
-        except oslo_vmw_exceptions.FileAlreadyExistsException:
-            pass
-
-    # Note(vbala) remove this method when we implement it in oslo.vmware
-    def _upload_vmdk(
-            self, read_handle, host, port, dc_name, ds_name, cookies,
-            upload_file_path, file_size, cacerts, timeout_secs):
-        write_handle = rw_handles.FileWriteHandle(host,
-                                                  port,
-                                                  dc_name,
-                                                  ds_name,
-                                                  cookies,
-                                                  upload_file_path,
-                                                  file_size,
-                                                  cacerts=cacerts)
-        image_transfer._start_transfer(read_handle, write_handle, timeout_secs)
-
-    def _disconnect(self, session, tmp_file_path, tmp_file, tmp_file_size,
-                    ds_ref, dc_ref, vmdk_path):
-        # The restored volume is in compressed (streamOptimized) format.
-        # So we upload it to a temporary location in vCenter datastore and copy
-        # the compressed vmdk to the volume vmdk. The copy operation
-        # decompresses the disk to a format suitable for attaching to Nova
-        # instances in vCenter.
-        dstore = datastore.get_datastore_by_ref(session, ds_ref)
-        ds_path = dstore.build_path(
-            VmdkConnector.TMP_IMAGES_DATASTORE_FOLDER_PATH,
-            os.path.basename(tmp_file_path))
-        self._create_temp_ds_folder(
-            session, six.text_type(ds_path.parent), dc_ref)
-
-        dc_name = session.invoke_api(
-                vim_util, 'get_object_property', session.vim, dc_ref, 'name')
-        cookies = session.vim.client.options.transport.cookiejar
-        cacerts = self._ca_file if self._ca_file else not self._insecure
-        self._upload_vmdk(
-                tmp_file, self._ip, self._port, dc_name, dstore.name, cookies,
-                ds_path.rel_path, tmp_file_size, cacerts,
-                self._timeout)
-
-        # Delete the current volume vmdk because the copy operation does not
-        # overwrite.
-        LOG.debug("Deleting %s", vmdk_path)
-        disk_mgr = session.vim.service_content.virtualDiskManager
-        task = session.invoke_api(session.vim,
-                                  'DeleteVirtualDisk_Task',
-                                  disk_mgr,
-                                  name=vmdk_path,
-                                  datacenter=dc_ref)
-        session.wait_for_task(task)
-
-        src = six.text_type(ds_path)
-        LOG.debug("Copying %(src)s to %(dest)s", {'src': src,
-                                                  'dest': vmdk_path})
-        task = session.invoke_api(session.vim,
-                                  'CopyVirtualDisk_Task',
-                                  disk_mgr,
-                                  sourceName=src,
-                                  sourceDatacenter=dc_ref,
-                                  destName=vmdk_path,
-                                  destDatacenter=dc_ref)
-        session.wait_for_task(task)
-
-        # Delete the compressed vmdk at the temporary location.
-        LOG.debug("Deleting %s", src)
-        file_mgr = session.vim.service_content.fileManager
-        task = session.invoke_api(session.vim,
-                                  'DeleteDatastoreFile_Task',
-                                  file_mgr,
-                                  name=src,
-                                  datacenter=dc_ref)
-        session.wait_for_task(task)
-
-    def _disconnect_with_import(self, import_data, volume_ops, backing,
-                                tmp_file, file_size):
+    def _get_write_handle(self, import_data, volume_ops, file_size):
         import_spec = volume_ops.get_import_spec(import_data)
         folder = vim_util.get_moref(import_data['folder'],
                                     'Folder')
         rp = vim_util.get_moref(import_data['resource_pool'],
                                 'ResourcePool')
 
-        volume_ops.delete_backing(backing)
-        write_handle = rw_handles.VmdkWriteHandle(
+        return rw_handles.VmdkWriteHandle(
             volume_ops._session,
             self._ip,
             self._port,
@@ -263,62 +167,33 @@ class VmdkConnector(initiator_connector.InitiatorConnector):
             import_spec,
             file_size,
             'POST')
-        image_transfer._start_transfer(tmp_file, write_handle, self._timeout)
 
     def disconnect_volume(self, connection_properties, device_info,
                           force=False, ignore_errors=False):
-        tmp_file_path = device_info['path']
-        if not os.path.exists(tmp_file_path):
-            msg = _("Vmdk: %s not found.") % tmp_file_path
-            raise exception.NotFound(message=msg)
-
+        vmdk_handle = device_info['path']
         session = None
-        try:
-            # We upload the temporary file to vCenter server only if it is
-            # modified after connect_volume.
-            if os.path.getmtime(tmp_file_path) > device_info['last_modified']:
-                self._load_config(connection_properties)
-                session = self._create_session()
-                backing = vim_util.get_moref(connection_properties['volume'],
-                                             "VirtualMachine")
-                # Currently there is no way we can restore the volume if it
-                # contains redo-log based snapshots (bug 1599026).
-                if self._snapshot_exists(session, backing):
-                    msg = (_("Backing of volume: %s contains one or more "
-                             "snapshots; cannot disconnect.") %
-                           connection_properties['volume_id'])
-                    raise exception.BrickException(message=msg)
+        if connection_properties['import_data']:
+            session = self._create_session()
+            volume_ops = VolumeOps(session)
+            backing = vim_util.get_moref(connection_properties['volume'],
+                                         "VirtualMachine")
+            new_backing = vmdk_handle.get_imported_vm()
+            # Currently there is no way we can restore the volume if it
+            # contains redo-log based snapshots (bug 1599026).
+            if self._snapshot_exists(session, backing):
+                msg = (_("Backing of volume: %s contains one or more "
+                         "snapshots; cannot disconnect.") %
+                       connection_properties['volume_id'])
+                volume_ops.delete_backing(new_backing)
+                raise exception.BrickException(message=msg)
 
-                ds_ref = vim_util.get_moref(
-                    connection_properties['datastore'], "Datastore")
-                dc_ref = vim_util.get_moref(
-                    connection_properties['datacenter'], "Datacenter")
-                vmdk_path = connection_properties['vmdk_path']
+            volume_ops.delete_backing(backing)
+            volume_ops.update_instance_uuid(new_backing,
+                                            connection_properties['volume_id'])
 
-                with open(tmp_file_path, "rb") as tmp_file:
-                    file_size = os.path.getsize(tmp_file_path)
-                    import_data = connection_properties.get('import_data')
-                    if import_data:
-                        import_data = connection_properties['import_data']
-                        import_data['profile_id'] = connection_properties[
-                            'profile_id']
-                        import_data['vm']['name'] = connection_properties[
-                            'name']
-                        import_data['vm']['uuid'] = connection_properties[
-                            'volume_id']
-                        self._disconnect_with_import(import_data,
-                                                     VolumeOps(session),
-                                                     backing,
-                                                     tmp_file,
-                                                     file_size)
-                    else:
-                        self._disconnect(session, tmp_file_path, tmp_file,
-                                         file_size, ds_ref, dc_ref,
-                                         vmdk_path)
-        finally:
-            os.remove(tmp_file_path)
-            if session:
-                session.logout()
+        vmdk_handle.close()
+        if session:
+            session.logout()
 
     def extend_volume(self, connection_properties):
         raise NotImplementedError
@@ -336,6 +211,16 @@ class VolumeOps:
 
         return vm_import_spec
 
+    def update_instance_uuid(self, vm_ref, uuid):
+        cf = self._session.vim.client.factory
+        config_spec = cf.create('ns0:VirtualMachineConfigSpec')
+        config_spec.instanceUuid = uuid
+        task = self._session.invoke_api(self._session.vim,
+                                        'ReconfigVM_Task',
+                                         vm_ref,
+                                         spec=config_spec)
+        self._session.wait_for_task(task)
+
     def get_vm_config_spec(self, import_data):
         vm = import_data['vm']
         cf = self._session.vim.client.factory
@@ -349,7 +234,6 @@ class VolumeOps:
         config_spec.memoryMB = vm['memory_mb']
         config_spec.files = vm_file_info
         config_spec.version = vm['vmx_version']
-        config_spec.instanceUuid = vm['uuid']
 
         extra_config = vm['extra_config']
         if extra_config:
